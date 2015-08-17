406d003c76efe7142b9bb3fe1d27323e21422de6
Duncan Coutts <duncan@community.haskell.org>
2015-08-17 14:21:39 +0100

    Add the grabWordXX ops for non-x86
    For big endian and arches that cannot do unaligned mem ops.

--- Data/Binary/Serialise/CBOR/ByteOrder.hs
+++ Data/Binary/Serialise/CBOR/ByteOrder.hs
@@ -10,13 +10,29 @@ import           GHC.ForeignPtr
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Internal as BS
 
+#include "MachDeps.h"
+
+#if i386_HOST_ARCH || x86_64_HOST_ARCH
+#define MEM_UNALIGNED_OPS
+#else
+import           Data.Bits ((.|.), shiftL)
+#endif
+
+#if WORD_SIZE_IN_BITS == 64
+#define ARCH_64bit
+#elif WORD_SIZE_IN_BITS == 32
+import           GHC.IntWord64 (wordToWord64#)
+#else
+#error expected WORD_SIZE_IN_BITS to be 32 or 64
+#endif
+
+
 {-# INLINE grabWord8 #-}
 grabWord8 :: Ptr () -> Word
 grabWord8 (Ptr ip#) =
     W# (indexWord8OffAddr# ip# 0#)
 
---TODO: implementations for big endian
---TODO: implementations for systems that don't do unaligned memory ops
+#if defined(MEM_UNALIGNED_OPS) && !defined(WORDS_BIGENDIAN)
 
 {-# INLINE grabWord16 #-}
 grabWord16 :: Ptr () -> Word
@@ -33,6 +49,84 @@ grabWord64 :: Ptr () -> Word64
 grabWord64 (Ptr ip#) =
     W64# (byteSwap64# (indexWord64OffAddr# ip# 0#))
 
+#elif defined(MEM_UNALIGNED_OPS) && defined(WORDS_BIGENDIAN)
+
+{-# INLINE grabWord16 #-}
+grabWord16 :: Ptr () -> Word
+grabWord16 (Ptr ip#) =
+    W# (indexWord16OffAddr# ip# 0#)
+
+{-# INLINE grabWord32 #-}
+grabWord32 :: Ptr () -> Word
+grabWord32 (Ptr ip#) =
+    W# (indexWord32OffAddr# ip# 0#)
+
+{-# INLINE grabWord64 #-}
+grabWord64 :: Ptr () -> Word64
+grabWord64 (Ptr ip#) =
+    W64# (indexWord64OffAddr# ip# 0#)
+
+#else
+
+{-# INLINE grabWord16 #-}
+grabWord16 :: Ptr () -> Word
+grabWord16 (Ptr ip#) =
+    case indexWord8OffAddr# ip# 0# of
+     w0# ->
+      case indexWord8OffAddr# ip# 1# of
+       w1# -> W# w0# `shiftL` 8 .|.
+              W# w1#
+
+{-# INLINE grabWord32 #-}
+grabWord32 :: Ptr () -> Word
+grabWord32 (Ptr ip#) =
+    case indexWord8OffAddr# ip# 0# of
+     w0# ->
+      case indexWord8OffAddr# ip# 1# of
+       w1# ->
+        case indexWord8OffAddr# ip# 2# of
+         w2# ->
+          case indexWord8OffAddr# ip# 3# of
+           w3# -> W# w0# `shiftL` 24 .|.
+                  W# w1# `shiftL` 16 .|.
+                  W# w2# `shiftL`  8 .|.
+                  W# w3#
+
+{-# INLINE grabWord64 #-}
+grabWord64 :: Ptr () -> Word64
+grabWord64 (Ptr ip#) =
+    case indexWord8OffAddr# ip# 0# of
+     w0# ->
+      case indexWord8OffAddr# ip# 1# of
+       w1# ->
+        case indexWord8OffAddr# ip# 2# of
+         w2# ->
+          case indexWord8OffAddr# ip# 3# of
+           w3# ->
+            case indexWord8OffAddr# ip# 4# of
+             w4# ->
+              case indexWord8OffAddr# ip# 5# of
+               w5# ->
+                case indexWord8OffAddr# ip# 6# of
+                 w6# ->
+                  case indexWord8OffAddr# ip# 7# of
+                   w7# -> w w0# `shiftL` 56 .|.
+                          w w1# `shiftL` 48 .|.
+                          w w2# `shiftL` 40 .|.
+                          w w3# `shiftL` 32 .|.
+                          w w4# `shiftL` 24 .|.
+                          w w5# `shiftL` 16 .|.
+                          w w6# `shiftL`  8 .|.
+                          w w7#
+  where
+#ifdef ARCH_64bit
+    w w# = W64# w#
+#else
+    w w# = W64# (wordToWord64# w#)
+#endif
+
+#endif
+
 {-# INLINE withBsPtr #-}
 withBsPtr :: (Ptr b -> a) -> ByteString -> a
 withBsPtr f (BS.PS (ForeignPtr addr# _fpc) off _len) = f (Ptr addr# `plusPtr` off)
@@ -42,6 +136,9 @@ unsafeHead :: ByteString -> Word8
 unsafeHead (BS.PS (ForeignPtr addr# _fpc) (I# off#) _len) =
     W8# (indexWord8OffAddr# addr# off#)
 
+--
+-- Half floats
+--
 
 {-# INLINE wordToFloat16 #-}
 wordToFloat16 :: Word -> Float
@@ -58,8 +155,13 @@ foreign import ccall unsafe "hs_binary_floatToHalf"
   floatToHalf :: Float -> CUShort
 
 
--- Casting words to floats. We have to go via a word because of the endian
--- issues.
+--
+-- Casting words to floats
+--
+
+-- We have to go via a word rather than reading directly from memory because of
+-- endian issues. A little endian machine cannot read a big-endian float direct
+-- from memory, so we read a word, bswap it and then convert to float.
 
 -- Currently there are no primops for casting word <-> float, see
 -- https://ghc.haskell.org/trac/ghc/ticket/4092
