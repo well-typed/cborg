11d58d5a8c8ef85687e1594a0b6d5d01b95e790f
Austin Seipp <aseipp@pobox.com>
2015-11-30 13:33:39 -0600

    Add a skeleton tutorial module
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ Data/Binary/Serialise/CBOR/Tutorial.hs
@@ -0,0 +1,49 @@
+-- |
+-- Module      : Data.Binary.Serialise.CBOR.Tutorial
+-- Copyright   : (c) Duncan Coutts 2015
+-- License     : BSD3-style (see LICENSE.txt)
+--
+-- Maintainer  : duncan@community.haskell.org
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+-- Lorem ipsum...
+--
+module Data.Binary.Serialise.CBOR.Tutorial
+       ( -- * Introduction
+         -- $introduction
+
+         -- ** The CBOR format
+         -- $cborformat
+
+         -- ** Encoding terms
+         -- $encoding
+
+         -- ** Decoding terms
+         -- $decoding
+
+       ) where
+
+{- $introduction
+
+Lorem ipsum...
+
+-}
+
+{- $cborformat
+
+Lorem ipsum...
+
+-}
+
+{- $encoding
+
+Lorem ipsum...
+
+-}
+
+{- $decoding
+
+Lorem ipsum...
+
+-}
