f3209d8cb9bc25b9e360d2093f16a7abbf25dacd
Austin Seipp <austin@well-typed.com>
2016-02-25 12:30:58 -0600

    FlatTerm: add a module header
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- Data/Binary/Serialise/CBOR/FlatTerm.hs
+++ Data/Binary/Serialise/CBOR/FlatTerm.hs
@@ -1,4 +1,16 @@
 {-# LANGUAGE CPP, MagicHash #-}
+
+-- |
+-- Module      : Data.Binary.Serialise.CBOR.FlatTerm
+-- Copyright   : (c) Duncan Coutts 2015
+-- License     : BSD3-style (see LICENSE.txt)
+--
+-- Maintainer  : duncan@community.haskell.org
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+-- Lorem ipsum...
+--
 module Data.Binary.Serialise.CBOR.FlatTerm (
     FlatTerm,
     TermToken(..),
