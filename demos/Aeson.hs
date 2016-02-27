a3cbcb010494df330d009c636381c230e5d2c671
Austin Seipp <austin@well-typed.com>
2016-02-27 00:28:25 -0600

    demo-aeson: get up and running in a basic manner
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- demos/Aeson.hs
+++ demos/Aeson.hs
@@ -3,28 +3,30 @@
 module Main
   ( main -- :: IO ()
   ) where
-
-import           Data.Aeson
-import           Data.Scientific
-import qualified Data.Vector as Vec
-import qualified Data.HashMap.Lazy as HashMap
-
-import           Data.Binary.Serialise.CBOR.Encoding
-import           Data.Binary.Serialise.CBOR.Decoding
-import           Data.Binary.Serialise.CBOR
-
 import           Data.Monoid
+import           System.FilePath
+import           System.Environment
 
 #if !MIN_VERSION_base(4,8,0)
 import           Control.Applicative
 #endif
 
-instance Serialise Value where
-  encode = encodeValue
-  decode = decodeValue
+import           Data.Aeson                          as Aeson
+import           Data.Aeson.Encode.Pretty            as Aeson.Pretty
+import           Data.Scientific
+import qualified Data.Vector                         as Vec
+import qualified Data.HashMap.Lazy                   as HashMap
+import qualified Data.ByteString.Lazy                as LB
+
+import qualified Data.Binary.Serialise.CBOR.Read     as CBOR.Read
+import qualified Data.Binary.Serialise.CBOR.Write    as CBOR.Write
+import           Data.Binary.Serialise.CBOR.Encoding
+import           Data.Binary.Serialise.CBOR.Decoding
+
+--------------------------------------------------------------------------------
+-- Encoder from JSON values to CBOR values
 
 encodeValue :: Value -> Encoding
-
 encodeValue (Object vs) = encodeMapLen (fromIntegral $ HashMap.size vs)
                        <> mconcat [ encodeString k <> encodeValue v
                                   | (k,v) <- HashMap.toList vs ]
@@ -37,6 +39,9 @@ encodeValue (Number n)  = case floatingOrInteger n of
                             Right i -> encodeInteger i
 encodeValue (Bool   b)  = encodeBool b
 encodeValue  Null       = encodeNull
+
+--------------------------------------------------------------------------------
+-- Decoder from CBOR values to JSON values
 
 decodeValue :: Decoder Value
 decodeValue = do
@@ -86,6 +91,32 @@ decodeObject n0 = do
 --------------------------------------------------------------------------------
 -- Driver program
 
+-- | Convert an arbitrary JSON file into CBOR format.
+jsonToCbor :: FilePath -> IO ()
+jsonToCbor file = do
+  bs <- LB.readFile file
+  case (Aeson.decode bs) of
+    Nothing -> fail "invalid JSON file!"
+    Just v  -> do
+      let encVal   = encodeValue v
+          cborFile = dropExtension file <.> ".cbor"
+      -- Now write the blob
+      LB.writeFile cborFile (CBOR.Write.toLazyByteString encVal)
+
+-- | Convert a CBOR file to JSON, and echo it to @stdout@.
+cborToJson :: FilePath -> IO ()
+cborToJson file = do
+  bs <- LB.readFile file
+  case (CBOR.Read.deserialiseFromBytes decodeValue bs) of
+    Left err -> fail $ "deserialization error: " ++ err
+    -- TODO FIXME: encoding nonsense
+    Right v  -> LB.putStrLn (Aeson.Pretty.encodePretty v)
+
+-- | Main entry point.
 main :: IO ()
 main = do
-  return ()
+  args <- getArgs
+  case args of
+    ("encode":file:_) -> jsonToCbor file
+    ("decode":file:_) -> cborToJson file
+    _ -> fail "invalid args: use 'encode <file>.json' or 'decode <file>.cbor'"
