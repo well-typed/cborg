c3fa0fea6b46ce9380d69bd9b6d5524a85c18555
Austin Seipp <austin@well-typed.com>
2016-04-04 14:19:07 -0500

    bench: Add vector instance benchmarks
    These are the benchmarks from #42, reworked to give the benchmarks
    slightly better names. The prior ones had a name like
    '500/encode/boxed', '5000/encode/boxed', and so on, meaning it was
    impossible to distinguish them by a prefix match, like if I wanted
    criterion to only run the boxed encoder tests.
    
    The new benchmarks have a form like:
    
       instance/vector/boxed/encode/500
       instance/vector/boxed/encode/5000
       instance/vector/boxed/encode/50000
       instance/vector/boxed/encode/500000
       instance/vector/boxed/decode/500
       instance/vector/boxed/decode/5000
       instance/vector/boxed/decode/50000
       instance/vector/boxed/decode/500000
    
    Which makes it much easier to look exactly at what you're improving
    by simply providing criterion the prefix of the benchmark name.
    
    Closes #42.
    
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- bench/Instances.hs
+++ bench/Instances.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Instances
   ( benchmarks -- :: [Benchmark]
   ) where
 
 import           Criterion.Main
 
+import qualified Instances.Vector as Vector
+
 benchmarks :: [Benchmark]
 benchmarks =
-  [
+  [ bgroup "vector" Vector.benchmarks
   ]
---
+++ bench/Instances/Vector.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Instances.Vector
+  ( benchmarks -- :: [Benchmark]
+  ) where
+
+import           Data.Proxy
+import           Criterion.Main
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector as V
+import qualified Data.ByteString.Lazy as BSL
+import           Data.Binary.Serialise.CBOR
+import           Control.DeepSeq (force)
+
+benchmarks :: [Benchmark]
+benchmarks =
+  [ bgroup "unboxed"
+      [ bgroup "encode"
+          [ vectorEncode unboxed (42 :: Int) 500
+          , vectorEncode unboxed (42 :: Int) 5000
+          , vectorEncode unboxed (42 :: Int) 50000
+          , vectorEncode unboxed (42 :: Int) 500000
+          ]
+      , bgroup "decode"
+          [ vectorDecode unboxed (42 :: Int) 500
+          , vectorDecode unboxed (42 :: Int) 5000
+          , vectorDecode unboxed (42 :: Int) 50000
+          , vectorDecode unboxed (42 :: Int) 500000
+          ]
+      ]
+  , bgroup "boxed"
+      [ bgroup "encode"
+          [ vectorEncode boxed (42 :: Int) 500
+          , vectorEncode boxed (42 :: Int) 5000
+          , vectorEncode boxed (42 :: Int) 50000
+          , vectorEncode boxed (42 :: Int) 500000
+          ]
+      , bgroup "decode"
+          [ vectorDecode boxed (42 :: Int) 500
+          , vectorDecode boxed (42 :: Int) 5000
+          , vectorDecode boxed (42 :: Int) 50000
+          , vectorDecode boxed (42 :: Int) 500000
+          ]
+      ]
+  ]
+  where
+    unboxed = Proxy :: Proxy VU.Vector
+    boxed   = Proxy :: Proxy V.Vector
+
+--------------------------------------------------------------------------------
+-- Encoding
+
+vectorEncode :: forall v a. (Serialise (v a), VG.Vector v a, Num a)
+  => Proxy v
+  -> a
+  -> Int
+  -> Benchmark
+vectorEncode Proxy x len
+  = bench (show len) $ benchEncode (asVector $ VG.replicate len x)
+  where
+    asVector v = v :: v a
+
+    benchEncode :: Serialise s => s -> Benchmarkable
+    benchEncode = whnf (BSL.length . serialise)
+    {-# INLINE benchEncode #-}
+
+--------------------------------------------------------------------------------
+-- Decoding
+
+vectorDecode :: forall v a. (Serialise (v a), VG.Vector v a, Num a)
+  => Proxy v
+  -> a
+  -> Int
+  -> Benchmark
+vectorDecode Proxy x len
+  = bench (show len) $ benchDecode (force $ serialise $ asVector $ VG.replicate len x)
+  where
+    asVector v = v :: v a
+
+    benchDecode :: BSL.ByteString -> Benchmarkable
+    benchDecode = whnf (VG.sum . asVector . deserialise)
+    {-# INLINE benchDecode #-}
