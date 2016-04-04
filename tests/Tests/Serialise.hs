e6a0d41eddf8309a26022752ba9670d7148956b2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-04-04 18:02:18 +0300

    Add Serialize instances for data types from Foreign.C.Types

--- tests/Tests/Serialise.hs
+++ tests/Tests/Serialise.hs
@@ -21,6 +21,7 @@ import           Data.Typeable
 #if !MIN_VERSION_base(4,8,0)
 import           Control.Applicative
 #endif
+import           Foreign.C.Types
 
 import           Test.QuickCheck
 import           Test.Tasty
@@ -122,6 +123,31 @@ testTree = testGroup "Serialise class"
       , mkTest (T :: T Float)
       , mkTest (T :: T Double)
       , mkTest (T :: T Char)
+      , mkTest (T :: T CChar)
+      , mkTest (T :: T CSChar)
+      , mkTest (T :: T CUChar)
+      , mkTest (T :: T CShort)
+      , mkTest (T :: T CUShort)
+      , mkTest (T :: T CInt)
+      , mkTest (T :: T CUInt)
+      , mkTest (T :: T CLong)
+      , mkTest (T :: T CULong)
+      , mkTest (T :: T CPtrdiff)
+      , mkTest (T :: T CSize)
+      , mkTest (T :: T CWchar)
+      , mkTest (T :: T CSigAtomic)
+      , mkTest (T :: T CLLong)
+      , mkTest (T :: T CULLong)
+      , mkTest (T :: T CIntPtr)
+      , mkTest (T :: T CUIntPtr)
+      , mkTest (T :: T CIntMax)
+      , mkTest (T :: T CUIntMax)
+      , mkTest (T :: T CClock)
+      , mkTest (T :: T CTime)
+      , mkTest (T :: T CUSeconds)
+      , mkTest (T :: T CSUSeconds)
+      , mkTest (T :: T CFloat)
+      , mkTest (T :: T CDouble)
       , mkTest (T :: T (Int, Char))
       , mkTest (T :: T (Int, Char, Bool))
       , mkTest (T :: T (Int, Char, Bool, String))
@@ -272,3 +298,102 @@ instance ( Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d, Arbitrary e
   arbitrary = return (,,,,,,)
           <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
           <*> arbitrary <*> arbitrary <*> arbitrary
+instance Arbitrary CChar where
+  arbitrary = CChar <$> arbitrary
+  shrink (CChar x) = CChar <$> shrink x
+
+instance Arbitrary CSChar where
+  arbitrary = CSChar <$> arbitrary
+  shrink (CSChar x) = CSChar <$> shrink x
+
+instance Arbitrary CUChar where
+  arbitrary = CUChar <$> arbitrary
+  shrink (CUChar x) = CUChar <$> shrink x
+
+instance Arbitrary CShort where
+  arbitrary = CShort <$> arbitrary
+  shrink (CShort x) = CShort <$> shrink x
+
+instance Arbitrary CUShort where
+  arbitrary = CUShort <$> arbitrary
+  shrink (CUShort x) = CUShort <$> shrink x
+
+instance Arbitrary CInt where
+  arbitrary = CInt <$> arbitrary
+  shrink (CInt x) = CInt <$> shrink x
+
+instance Arbitrary CUInt where
+  arbitrary = CUInt <$> arbitrary
+  shrink (CUInt x) = CUInt <$> shrink x
+
+instance Arbitrary CLong where
+  arbitrary = CLong <$> arbitrary
+  shrink (CLong x) = CLong <$> shrink x
+
+instance Arbitrary CULong where
+  arbitrary = CULong <$> arbitrary
+  shrink (CULong x) = CULong <$> shrink x
+
+instance Arbitrary CPtrdiff where
+  arbitrary = CPtrdiff <$> arbitrary
+  shrink (CPtrdiff x) = CPtrdiff <$> shrink x
+
+instance Arbitrary CSize where
+  arbitrary = CSize <$> arbitrary
+  shrink (CSize x) = CSize <$> shrink x
+
+instance Arbitrary CWchar where
+  arbitrary = CWchar <$> arbitrary
+  shrink (CWchar x) = CWchar <$> shrink x
+
+instance Arbitrary CSigAtomic where
+  arbitrary = CSigAtomic <$> arbitrary
+  shrink (CSigAtomic x) = CSigAtomic <$> shrink x
+
+instance Arbitrary CLLong where
+  arbitrary = CLLong <$> arbitrary
+  shrink (CLLong x) = CLLong <$> shrink x
+
+instance Arbitrary CULLong where
+  arbitrary = CULLong <$> arbitrary
+  shrink (CULLong x) = CULLong <$> shrink x
+
+instance Arbitrary CIntPtr where
+  arbitrary = CIntPtr <$> arbitrary
+  shrink (CIntPtr x) = CIntPtr <$> shrink x
+
+instance Arbitrary CUIntPtr where
+  arbitrary = CUIntPtr <$> arbitrary
+  shrink (CUIntPtr x) = CUIntPtr <$> shrink x
+
+instance Arbitrary CIntMax where
+  arbitrary = CIntMax <$> arbitrary
+  shrink (CIntMax x) = CIntMax <$> shrink x
+
+instance Arbitrary CUIntMax where
+  arbitrary = CUIntMax <$> arbitrary
+  shrink (CUIntMax x) = CUIntMax <$> shrink x
+
+instance Arbitrary CClock where
+  arbitrary = CClock <$> arbitrary
+  shrink (CClock x) = CClock <$> shrink x
+
+instance Arbitrary CTime where
+  arbitrary = CTime <$> arbitrary
+  shrink (CTime x) = CTime <$> shrink x
+
+instance Arbitrary CUSeconds where
+  arbitrary = CUSeconds <$> arbitrary
+  shrink (CUSeconds x) = CUSeconds <$> shrink x
+
+instance Arbitrary CSUSeconds where
+  arbitrary = CSUSeconds <$> arbitrary
+  shrink (CSUSeconds x) = CSUSeconds <$> shrink x
+
+instance Arbitrary CFloat where
+  arbitrary = CFloat <$> arbitrary
+  shrink (CFloat x) = CFloat <$> shrink x
+
+instance Arbitrary CDouble where
+  arbitrary = CDouble <$> arbitrary
+  shrink (CDouble x) = CDouble <$> shrink x
