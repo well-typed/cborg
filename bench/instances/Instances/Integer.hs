797ae1e19a19984d14a332ea170bedebf37442b0
Michal Terepeta <michal.terepeta@gmail.com>
2016-04-10 15:02:37 +0200

    bench/instances: add Integer benchmarks
    Part of pr #47.
    
    Signed-off-by: Michal Terepeta <michal.terepeta@gmail.com>
    Signed-off-by: Austin Seipp <austin@well-typed.com>

---
+++ bench/instances/Instances/Integer.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Instances.Integer
+  ( benchmarks -- :: [Benchmark]
+  ) where
+
+import           Criterion.Main
+import qualified Data.ByteString.Lazy   as BS
+import qualified Data.Vector            as Vector
+
+import           Data.Binary.Serialise.CBOR
+
+benchmarks :: [Benchmark]
+benchmarks =
+  [ bench "small" (nf go integerDataSmall)
+  , bench "large" (nf go integerDataLarge)
+  ]
+  where
+    go = BS.length . serialise
+    integerDataSmall = Vector.replicate (100 :: Int) (10 :: Integer)
+    integerDataLarge = Vector.replicate (100 :: Int) ((2 :: Integer)^(70 :: Integer))
+
--- bench/instances/Main.hs
+++ bench/instances/Main.hs
@@ -4,13 +4,15 @@ module Main
 
 import           Criterion.Main (bgroup, defaultMain)
 
-import qualified Instances.Time   as Time
-import qualified Instances.Vector as Vector
+import qualified Instances.Integer as Integer
+import qualified Instances.Time    as Time
+import qualified Instances.Vector  as Vector
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "time"   Time.benchmarks
-  , bgroup "vector" Vector.benchmarks
+  [ bgroup "integer" Integer.benchmarks
+  , bgroup "time"    Time.benchmarks
+  , bgroup "vector"  Vector.benchmarks
   ]
