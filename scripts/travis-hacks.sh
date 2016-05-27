3ff4c1b238bfc3b0dd17772ffd7b88a4ddcd29d6
Austin Seipp <austin@well-typed.com>
2016-05-27 00:01:33 +0000

    travis: comments only
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- scripts/travis-hacks.sh
+++ scripts/travis-hacks.sh
@@ -7,6 +7,11 @@ set -e
 set -x
 unset CDPATH
 
+# Currently, the benchmarks take way too much memory and time to
+# compile, so we clone a version of cereal in with a particular
+# compile-time performance fix, which greatly enhances the memory
+# use. This allows GHC 8.0+ to reliably build the benchmarks on
+# Travis.
 git clone https://github.com/GaloisInc/cereal.git
 cd cereal && git reset --hard 4124881545bcc88951d1a10bb5e34e75520c380e && cd ..
 echo "packages: cereal/" > cabal.project.local
