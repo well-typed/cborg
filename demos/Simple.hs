b70e325eb43d52c38701ef340684eaa71c70df63
Austin Seipp <austin@well-typed.com>
2017-05-02 08:37:12 -0500

    demos: rewrite Simple to use common style
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- demos/Simple.hs
+++ demos/Simple.hs
@@ -1,15 +1,16 @@
--- This is indended to be about the simplest possible use of
--- Data.Binary.Serialise.
+-- This is indended to be the simplest possible use of
+-- Data.Binary.Serialise.CBOR.
+module Main
+  ( main -- :: IO ()
+  ) where
 
-import Data.Binary.Serialise.CBOR as CBOR
-import qualified Data.ByteString.Lazy as Bs
+import           Data.Binary.Serialise.CBOR as CBOR
+import qualified Data.ByteString.Lazy       as BL
 
 a :: [ Int ] -- full type definition required
-a = [1,2,3]
+a = [ 1, 2, 3 ]
 
--- we don't output to the terminal because the byte string created will
--- contain non 7-bit ASCII characters.  
-
-main=Bs.writeFile "outfile.cbor" a_serialised
-     where
-       a_serialised = CBOR.serialise a
+-- Don't output to the terminal: the created ByteString will contain non 7-bit
+-- ASCII characters.
+main :: IO ()
+main = BL.writeFile "outfile.cbor" (CBOR.serialise a)
