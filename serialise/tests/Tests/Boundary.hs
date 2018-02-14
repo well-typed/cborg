d40fc204b014d81432746f847d92587de7df0be0
Andrzej Rybczak <andrzej@rybczak.net>
2018-02-14 16:35:35 +0100

    Add map/list length tests

--- serialise/tests/Tests/Boundary.hs
+++ serialise/tests/Tests/Boundary.hs
@@ -11,10 +11,10 @@ module Tests.Boundary
 
 import           Data.Either
 import           Data.Int
-import           Data.Typeable
 import           Data.Word
 
 import           Codec.CBOR.Decoding
+import           Codec.CBOR.Encoding
 import           Codec.CBOR.Read
 import           Codec.CBOR.Write
 import           Codec.Serialise.Class
@@ -25,6 +25,26 @@ import           Test.Tasty.QuickCheck
 #if !MIN_VERSION_base(4,8,0)
 import           Control.Applicative
 #endif
+
+-- | Generate values of type 'a' embedded within (usually larger) type 'r' with
+-- upped probabilities of getting neighbourhood of bounds of 'a'.
+arbitraryWithBounds
+  :: forall a r. (Bounded a, Integral a, Num r, Arbitrary r)
+  => a -> Gen r
+arbitraryWithBounds _ = frequency
+  [ (70, arbitrary)
+  -- Boundaries
+  , (5, pure $ fromIntegral (minBound     :: a))
+  , (5, pure $ fromIntegral (maxBound     :: a))
+  -- Near boundaries, in range
+  , (5, pure $ fromIntegral (minBound + 1 :: a))
+  , (5, pure $ fromIntegral (maxBound - 1 :: a))
+  -- Near boundaries, out of range (note: might overflow)
+  , (5, pure $ fromIntegral (minBound :: a) - 1)
+  , (5, pure $ fromIntegral (maxBound :: a) + 1)
+  ]
+
+----------------------------------------
 
 -- | Wrapper for bounded, integral type 'a' that potentially contains values
 -- outside of range of 'a'.
@@ -47,30 +67,20 @@ instance Show (BRep a) => Show (B a) where
 
 instance (Arbitrary (BRep a), Num (BRep a), Bounded a, Integral a
          ) => Arbitrary (B a) where
-  arbitrary = B <$> frequency
-    [ (490, arbitrary)
-    -- Boundaries
-    , (85, pure $ fromIntegral (minBound     :: a))
-    , (85, pure $ fromIntegral (maxBound     :: a))
-    -- Near boundaries, in range
-    , (85, pure $ fromIntegral (minBound + 1 :: a))
-    , (85, pure $ fromIntegral (maxBound - 1 :: a))
-    -- Near boundaries, out of range (note: might overflow)
-    , (85, pure $ fromIntegral (minBound :: a) - 1)
-    , (85, pure $ fromIntegral (maxBound :: a) + 1)
-    ]
+  arbitrary = B <$> arbitraryWithBounds (undefined::a)
 
 -- | Check if deserialisation of values of type 'a' deals properly with the ones
--- that are out of range, i.e. fails to decode them.
+-- out of range, i.e. fails to decode them.
 boundaryTest
-  :: forall a rep. (Bounded a, Integral a, rep ~ BRep a,
+  :: forall a rep. (Bounded a, Integral a, Show a, rep ~ BRep a,
                     Ord rep, Num rep, Serialise rep)
   => (forall s. Decoder s a)
   -> B a
-  -> Bool
-boundaryTest dec a = if outsideRange
-                     then isLeft  a'
-                     else isRight a'
+  -> Property
+boundaryTest dec a =
+  counterexample (show a') $ if outsideRange
+                             then isLeft  a'
+                             else isRight a'
   where
     a' = deserialiseFromBytes dec . toLazyByteString . encode $ unB a
 
@@ -78,28 +88,75 @@ boundaryTest dec a = if outsideRange
                 || unB a > fromIntegral (maxBound :: a)
 
 mkBoundaryTest
-  :: forall a rep. (Bounded a, Integral a, Typeable a, rep ~ BRep a,
+  :: forall a rep. (Bounded a, Integral a, Show a, rep ~ BRep a,
                     Arbitrary rep, Ord rep, Show rep, Num rep, Serialise rep)
-  => (forall s. Decoder s a)
+  => String
+  -> (forall s. Decoder s a)
   -> (forall s. Decoder s a)
   -> [TestTree]
-mkBoundaryTest dec decCan =
-  [ testProperty (show a)                   $ boundaryTest dec
-  , testProperty (show a ++ " (canonical)") $ boundaryTest decCan
+mkBoundaryTest aName dec decCan =
+  [ testProperty aName                     $ boundaryTest dec
+  , testProperty (aName ++ " (canonical)") $ boundaryTest decCan
   ]
+
+----------------------------------------
+
+-- | Wrapper for list/map length.
+newtype Len = Len { unLen :: Word }
+
+instance Show Len where
+  showsPrec p = showsPrec p . unLen
+
+instance Arbitrary Len where
+  arbitrary = Len <$> arbitraryWithBounds (undefined::Int)
+
+-- | Check if deserialisation of map/list length deals properly with the ones
+-- out of range, i.e. fails to decode them.
+lenBoundaryTest
+  :: (Word -> Encoding)
+  -> (forall s. Decoder s Int)
+  -> Len
+  -> Property
+lenBoundaryTest enc dec a =
+  counterexample (show a') $ if outsideRange
+                             then isLeft  a'
+                             else isRight a'
   where
-    a = typeOf (undefined::a)
+    a' = deserialiseFromBytes dec . toLazyByteString . enc $ unLen a
+
+    outsideRange = fromIntegral (unLen a) < (0::Int)
+
+mkLenBoundaryTest
+  :: String
+  -> (Word -> Encoding)
+  -> (forall s. Decoder s Int)
+  -> (forall s. Decoder s Int)
+  -> [TestTree]
+mkLenBoundaryTest aName enc dec decCan =
+  [ testProperty aName                     $ lenBoundaryTest enc dec
+  , testProperty (aName ++ " (canonical)") $ lenBoundaryTest enc decCan
+  ]
+
 
 testTree :: TestTree
 testTree = testGroup "Boundary checks" $ concat
-  [ mkBoundaryTest decodeWord   decodeWordCanonical
-  , mkBoundaryTest decodeWord8  decodeWord8Canonical
-  , mkBoundaryTest decodeWord16 decodeWord16Canonical
-  , mkBoundaryTest decodeWord32 decodeWord32Canonical
-  , mkBoundaryTest decodeWord64 decodeWord64Canonical
-  , mkBoundaryTest decodeInt    decodeIntCanonical
-  , mkBoundaryTest decodeInt8   decodeInt8Canonical
-  , mkBoundaryTest decodeInt16  decodeInt16Canonical
-  , mkBoundaryTest decodeInt32  decodeInt32Canonical
-  , mkBoundaryTest decodeInt64  decodeInt64Canonical
+  [ mkBoundaryTest "Word"      decodeWord      decodeWordCanonical
+  , mkBoundaryTest "Word8"     decodeWord8     decodeWord8Canonical
+  , mkBoundaryTest "Word16"    decodeWord16    decodeWord16Canonical
+  , mkBoundaryTest "Word32"    decodeWord32    decodeWord32Canonical
+  , mkBoundaryTest "Word64"    decodeWord64    decodeWord64Canonical
+  , mkBoundaryTest "Int"       decodeInt       decodeIntCanonical
+  , mkBoundaryTest "Int8"      decodeInt8      decodeInt8Canonical
+  , mkBoundaryTest "Int16"     decodeInt16     decodeInt16Canonical
+  , mkBoundaryTest "Int32"     decodeInt32     decodeInt32Canonical
+  , mkBoundaryTest "Int64"     decodeInt64     decodeInt64Canonical
+
+  , mkLenBoundaryTest "ListLen" encodeListLen decodeListLen decodeListLenCanonical
+  , mkLenBoundaryTest "MapLen"  encodeMapLen  decodeMapLen  decodeMapLenCanonical
+
+  {-, mkBoundaryTest "NegWord"   decodeNegWord   decodeNegWordCanonical
+  , mkBoundaryTest "NegWord64" decodeNegWord64 decodeNegWord64Canonical
+  , mkBoundaryTest "Tag"       decodeTag       decodeTagCanonical
+  , mkBoundaryTest "Tag64"     decodeTag64     decodeTag64Canonical
+  , mkBoundaryTest "Simple"    decodeSimple    decodeSimpleCanonical-}
   ]
