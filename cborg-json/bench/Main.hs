2d96e8f7f8259a32155931d342e2bdb21feef01f
Alex Mason <Axman6@gmail.com>
2019-06-15 00:30:57 +1000

    Add strict encoding test

--- cborg-json/bench/Main.hs
+++ cborg-json/bench/Main.hs
@@ -6,13 +6,13 @@ module Main
   ( main -- :: IO ()
   ) where
 
-import           Criterion.Main (bgroup, defaultMain, bench, nf)
+import           Criterion.Main (bgroup, defaultMain, bench, nf, whnf)
 import           Data.ByteString.Lazy (ByteString, fromStrict)
 import           Data.FileEmbed (embedFile)
 import           Data.Aeson (Value, decode, encode)
 import           Codec.CBOR.JSON (encodeValue, decodeValue)
 import           Codec.CBOR.Read (deserialiseFromBytes)
-import           Codec.CBOR.Write (toLazyByteString)
+import           Codec.CBOR.Write (toLazyByteString, toStrictByteString)
 -- import           Paths_app
 
 largeJSONBytes :: ByteString
@@ -27,12 +27,13 @@ largeCBORBytes = toLazyByteString $ encodeValue largeJSON
 main :: IO ()
 main = defaultMain
     [ bgroup "Decode"
-        [ bench "JSON" (nf (decode @Value) largeJSONBytes)
-        , bench "CBOR" (nf (deserialiseFromBytes (decodeValue False)) largeCBORBytes)
-        , bench "CBOR lenient" (nf (deserialiseFromBytes (decodeValue True)) largeCBORBytes)
+        [ bench "JSON" (whnf (decode @Value) largeJSONBytes)
+        , bench "CBOR" (whnf (deserialiseFromBytes (decodeValue False)) largeCBORBytes)
+        , bench "CBOR lenient" (whnf (deserialiseFromBytes (decodeValue True)) largeCBORBytes)
         ]
     , bgroup "Encode"
         [ bench "JSON" (nf encode largeJSON)
         , bench "CBOR" (nf (toLazyByteString . encodeValue) largeJSON)
+        , bench "CBOR strict" (whnf (toStrictByteString . encodeValue) largeJSON)
         ]
     ]
