2b9e4760ce754b1aa6b3fd0b435ce5ccfe7bf9b5
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-06-26 17:22:21 -0400

    primitive-0.7.0.0 support

--- cborg/src/Codec/CBOR/ByteArray/Sliced.hs
+++ cborg/src/Codec/CBOR/ByteArray/Sliced.hs
@@ -35,7 +35,9 @@ import Control.Monad.ST
 import System.IO.Unsafe
 
 import qualified Data.Primitive.ByteArray as Prim
+#if !MIN_VERSION_primitive(0,7,0)
 import           Data.Primitive.Types (Addr(..))
+#endif
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Unsafe as BS
 import qualified Data.ByteString.Short as BSS
@@ -66,7 +68,11 @@ toByteString sba =
     $ BS.unsafePackCStringFinalizer ptr (sizeofSlicedByteArray sba) (touch pinned)
   where
     pinned = toPinned sba
+#if MIN_VERSION_primitive(0,7,0)
+    !(Ptr addr#) = Prim.byteArrayContents pinned
+#else
     !(Addr addr#) = Prim.byteArrayContents pinned
+#endif
     ptr = Ptr addr#
 
 toPinned :: SlicedByteArray -> Prim.ByteArray
