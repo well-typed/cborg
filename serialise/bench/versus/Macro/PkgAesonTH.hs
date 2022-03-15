e7b58c5129fd33ae7866c66dcb3efc362dc1215d
Ryan Scott <ryan.gl.scott@gmail.com>
2022-03-15 16:45:45 -0400

    Make the serialize benchmarks compile with GHC 9+
    Fixes #281.
    
    Signed-off-by: Ryan Scott <ryan.gl.scott@gmail.com>

--- serialise/bench/versus/Macro/PkgAesonTH.hs
+++ serialise/bench/versus/Macro/PkgAesonTH.hs
@@ -8,12 +8,6 @@ import Data.Aeson.TH as Aeson
 import Data.ByteString.Lazy as BS
 import Data.Maybe
 
-serialise :: [GenericPackageDescription] -> BS.ByteString
-serialise pkgs = Aeson.encode pkgs
-
-deserialise :: BS.ByteString -> [GenericPackageDescription]
-deserialise = fromJust . Aeson.decode'
-
 deriveJSON defaultOptions ''Version
 deriveJSON defaultOptions ''PackageName
 deriveJSON defaultOptions ''PackageId
@@ -21,29 +15,35 @@ deriveJSON defaultOptions ''VersionRange
 deriveJSON defaultOptions ''Dependency
 deriveJSON defaultOptions ''CompilerFlavor
 deriveJSON defaultOptions ''License
-deriveJSON defaultOptions ''SourceRepo
 deriveJSON defaultOptions ''RepoKind
 deriveJSON defaultOptions ''RepoType
+deriveJSON defaultOptions ''SourceRepo
 deriveJSON defaultOptions ''BuildType
-deriveJSON defaultOptions ''Library
-deriveJSON defaultOptions ''Executable
-deriveJSON defaultOptions ''TestSuite
-deriveJSON defaultOptions ''TestSuiteInterface
-deriveJSON defaultOptions ''TestType
-deriveJSON defaultOptions ''Benchmark
-deriveJSON defaultOptions ''BenchmarkInterface
-deriveJSON defaultOptions ''BenchmarkType
-deriveJSON defaultOptions ''BuildInfo
 deriveJSON defaultOptions ''ModuleName
 deriveJSON defaultOptions ''Language
-deriveJSON defaultOptions ''Extension
 deriveJSON defaultOptions ''KnownExtension
+deriveJSON defaultOptions ''Extension
+deriveJSON defaultOptions ''BuildInfo
+deriveJSON defaultOptions ''Library
+deriveJSON defaultOptions ''Executable
+deriveJSON defaultOptions ''TestType
+deriveJSON defaultOptions ''TestSuiteInterface
+deriveJSON defaultOptions ''TestSuite
+deriveJSON defaultOptions ''BenchmarkType
+deriveJSON defaultOptions ''BenchmarkInterface
+deriveJSON defaultOptions ''Benchmark
 deriveJSON defaultOptions ''PackageDescription
 deriveJSON defaultOptions ''OS
 deriveJSON defaultOptions ''Arch
-deriveJSON defaultOptions ''Flag
 deriveJSON defaultOptions ''FlagName
+deriveJSON defaultOptions ''Flag
+deriveJSON defaultOptions ''Condition
 deriveJSON defaultOptions ''CondTree
 deriveJSON defaultOptions ''ConfVar
-deriveJSON defaultOptions ''Condition
 deriveJSON defaultOptions ''GenericPackageDescription
+
+serialise :: [GenericPackageDescription] -> BS.ByteString
+serialise pkgs = Aeson.encode pkgs
+
+deserialise :: BS.ByteString -> [GenericPackageDescription]
+deserialise = fromJust . Aeson.decode'
