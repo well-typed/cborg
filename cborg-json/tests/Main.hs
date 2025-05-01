ad4927388efab6994589384a421c96f4caeafe15
Andrea Vezzosi <andrea@well-typed.com>
2025-05-01 10:46:07 +0200

    cborg-json: test for indef length map decoding as Object

--- cborg-json/tests/Main.hs
+++ cborg-json/tests/Main.hs
@@ -2,11 +2,16 @@
 
 module Main (main) where
 
+import           Codec.CBOR.Encoding
 import           Codec.CBOR.JSON
 import           Codec.CBOR.Read
-import           Data.Aeson (Value (String))
+import           Codec.CBOR.Write
+import           Data.Aeson (toJSON, Value (String))
 import qualified Data.ByteString.Base16 as HEX
 import           Data.ByteString.Lazy (fromStrict)
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Text as T
 import           Test.Tasty (TestTree, defaultMain, testGroup)
 import           Test.Tasty.HUnit (testCase, (@=?))
 
@@ -23,5 +28,17 @@ tests =
         [ testCase "decode variable ByteString as Base62Url String" $
             Right ("", String "MDEy") @=? deserialiseFromBytes (decodeValue True)
                                               (fromStrict . either error id $ HEX.decode "5803303132")
+        , testCase "decode Object encoded as indefinite length Map" $
+            Right ("", toJSON testMap) @=? deserialiseFromBytes (decodeValue True)
+                                              (toLazyByteString $ encodeMapIndef testMap)
         ]
     ]
+
+testMap :: Map T.Text Value
+testMap = Map.fromList $ [ (T.pack $ show n, toJSON (n :: Int)) | n <- [0..10]]
+
+encodeMapIndef :: Map T.Text Value -> Encoding
+encodeMapIndef vs =
+     encodeMapLenIndef
+  <> Map.foldrWithKey (\k v r -> encodeString k <> encodeValue v <> r) mempty vs
+  <> encodeBreak
